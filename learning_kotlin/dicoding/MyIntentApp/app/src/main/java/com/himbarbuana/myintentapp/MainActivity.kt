eaf10cc67ee52da5de3a578f20634cd8d6ed6d55
himbarbuana <mhimbarbuana@gmail.com>
2024-12-29 20:50:28 +0700

    Add latihan explicit intent

---
+++ learning_kotlin/dicoding/MyIntentApp/app/src/main/java/com/himbarbuana/myintentapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.himbarbuana.myintentapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
+    }
+}
